921f5f8f6e532e34ddfaed7fa097fe590f394d96
Kira <me@imkira.com>
2017-11-24 11:27:57 +0800

    feat(example): add example for basic api project

---
+++ examples/api-basic/api/v1/install.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2017 Caicloud Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"github.com/caicloud/nirvana/definition"
+	"github.com/caicloud/nirvana/web"
+)
+
+var descriptors = []definition.Descriptor{}
+
+func register(ds ...definition.Descriptor) {
+	descriptors = append(descriptors, ds...)
+}
+
+func Install(s web.Server) {
+	v1 := definition.Descriptor{
+		Path:        "/api/v1",
+		Description: "It contains all APIs in v1",
+		Consumes:    []string{"application/json"},
+		Produces:    []string{"application/json"},
+		Children:    descriptors,
+	}
+	if err := s.AddDescriptors(v1); err != nil {
+		panic(err)
+	}
+}
