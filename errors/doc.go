ee773f889ec3185bab3b6aa81bbc05f7675cf642
caitong93 <caitong93@gmail.com>
2017-12-19 21:45:30 +0800

    dep ensure

--- errors/doc.go
+++ errors/doc.go
@@ -1,24 +1,17 @@
-// Package errors is designed to be the default error returned by nirvana.
-// Recommended usage example:
-//
-// import "github.com/caicloud/nirvana/errors"
+/*
+Copyright 2017 Caicloud Authors
 
-// type App struct {}
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-// var AppIsNotFound = errors.NotFound.NewFactory(
-// 	errors.Reason("app-admin:app-not-found"),
-// 	"server can't find app ${appName} in partition ${partitionName}",
-// )
+    http://www.apache.org/licenses/LICENSE-2.0
 
-// func GetApp() (*App, error) {
-// 	return nil, AppIsNotFound.New("mongodb", "test")
-// }
-
-// func main() {
-// 	_, err := GetApp()
-// 	if AppIsNotFound.CanNew(err) {
-// 		// Do something
-// 	}
-// }
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package errors
--- validator/doc.go
+++ validator/doc.go
@@ -1,27 +1,17 @@
-// Package validator usage:
+/*
+Copyright 2017 Caicloud Authors
 
-// type Application struct {
-// 	Name      string `json:"name" validate:"required,printascii"`
-// 	Namespace string `json:"namespace"`
-// }
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-// Definitions: []definition.Definition{
-// 	{
-// 		Method:   definition.Create,
-// 		Function: Handle,
-// 		Parameters: []definition.Parameter{
-// 			{
-// 				Source:    definition.Query,
-// 				Name:      "target1",
-// 				Operators: []definition.Operator{validator.Var("gt=0,lt=10")},
-// 			},
-// 			{
-// 				Source: definition.Body,
-// 				Name:   "app",
-// 				Operators: []definition.Operator{validator.Struct()},
-// 			},
-// 		},
-// 	},
-// },
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package validator
