71f6da66451204ecb89354b9eb98bd514705ee90
Jiayu Liu <jiayu@caicloud.io>
2017-12-20 13:48:51 +0800

    feat: add instrumentation and profiling endpoints (#48)
    * dep ensure
    
    * dep prune
    
    * feat: add pprof and prometheus metrics endpoints
    
    * run ./hack/update-gofmt.sh

--- cmd/flag-gen/types/types.go
+++ cmd/flag-gen/types/types.go
@@ -16,24 +16,26 @@ limitations under the License.
 
 package types
 
+import "time"
+
 // go:generate flag-gen -i github.com/caicloud/nirvana/cmd/flag-gen/types -o github.com/caicloud/nirvana/cli
 
-// TODO: why not used?
-// FlagTypes ...
-// type FlagTypes struct {
-// 	a bool
-// 	// b []bool not support now
-// 	c time.Duration
-// 	d float32
-// 	e float64
-// 	f int
-// 	// g []int not support now
-// 	j int32
-// 	k int64
-// 	l string
-// 	m []string
-// 	n uint
-// 	// o []uint not support now
-// 	r uint32
-// 	s uint64
-// }
+// FlagTypes is an example struct to be consumed by flag-gen
+// nolint: megacheck
+type FlagTypes struct {
+	a bool
+	// b []bool not support now
+	c time.Duration
+	d float32
+	e float64
+	f int
+	// g []int not support now
+	j int32
+	k int64
+	l string
+	m []string
+	n uint
+	// o []uint not support now
+	r uint32
+	s uint64
+}
