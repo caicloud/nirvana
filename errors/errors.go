b2e1567c5f7809f61603236ebd261cff374c8060
Kira <me@imkira.com>
2017-12-29 14:29:59 +0800

    refactor(errors): refactor error factory structure

--- errors/errors.go
+++ errors/errors.go
@@ -27,85 +27,76 @@ import (
 // Reason "Status:Sleep" may map to Format "${Name} is sleeping now"
 type Reason string
 
-// message was the result returned to client.
-// Exp:
+// Factory can create error from a fixed format.
+type Factory interface {
+	// Error generates an error from v.
+	Error(v ...interface{}) error
+	// Derived checks if an error was derived by current factory.
+	Derived(e error) bool
+}
+
+// message can be marshaled for transferring.
+// Example for json:
 // {
-//   "message": "name japari is too short",
-//   "reason": "monitoring:CreateDashboardNameTooShort",
+//   "message": "name of something is to short",
+//   "reason": "SomeModule:NameTooShort",
 //   "data": {
-//     "name": "japari"
+//     "name": "something"
 //   }
 // }
 type message struct {
-	Reason  Reason            `json:"reason,omitempty"`
-	Message string            `json:"message"`
-	Data    map[string]string `json:"data,omitempty"`
+	// Reason is a unique key for an error in global environment.
+	// Following format is recommended:
+	//   `MuduleName[:SubmoduleName]:ShortErrorDescription`
+	Reason Reason `json:"reason,omitempty"`
+	// Message contains the detailed descrtiption of an error.
+	Message string `json:"message"`
+	// Data is used for i18n.
+	Data map[string]string `json:"data,omitempty"`
 }
 
+// err implements error interface.
 type err struct {
 	message
-	code   int
-	format string
+	factory *factory
 }
 
+// Code returns status code of the error.
 func (e *err) Code() int {
-	return e.code
+	return e.factory.code
 }
 
+// Message returns detailed message of the error.
 func (e *err) Message() interface{} {
 	return e.message
 }
 
+// Error returns error description.
 func (e *err) Error() string {
 	return e.message.Message
 }
 
 // Factory is an error factory.
-type Factory struct {
+type factory struct {
 	code   int
 	reason Reason
 	format string
 }
 
-// New generates an error.
-func (f *Factory) New(v ...interface{}) error {
+// Error generates an error from v.
+func (f *factory) Error(v ...interface{}) error {
 	msg := message{Reason: f.reason}
 	msg.Message, msg.Data = expand(f.format, v...)
 	return &err{
 		message: msg,
-		code:    f.code,
-		format:  f.format,
+		factory: f,
 	}
 }
 
-// CanNew checks whether f is able to New an error which has the same code, reason and format with e.
-func (f *Factory) CanNew(e error) bool {
-	x, ok := e.(*err)
-	if !ok {
-		return false
-	}
-	return f.code == x.code && f.reason == x.message.Reason && f.format == x.format
-}
-
-// Type maps to http code.
-// And it can be used to make an error factory.
-type Type struct {
-	code int
-}
-
-// NewType creates a new Type with code.
-func NewType(code int) *Type {
-	return &Type{code: code}
-}
-
-// NewFactory creates a factory to generate errors with predefined format.
-func (t *Type) NewFactory(reason Reason, format string) *Factory {
-	return &Factory{code: t.code, reason: reason, format: format}
-}
-
-// NewRaw creates an error which composed by code, reason and formated message in one call.
-func NewRaw(code int, reason Reason, format string, v ...interface{}) error {
-	return NewType(code).NewFactory(reason, format).New(v...)
+// Derived checks if an error was derived by current factory.
+func (f *factory) Derived(e error) bool {
+	origin, ok := e.(*err)
+	return ok && origin.factory == f
 }
 
 // expand expands a format string like "name ${name} is too short" to "name japari is too short"
