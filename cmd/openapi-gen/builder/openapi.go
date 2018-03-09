3d3bf2f1bb6bbfb4345932330724bf405989be94
Bo Liu <liubog2008@gmail.com>
2018-03-09 08:24:10 +0800

    fix(openapi): fix path item overriding problem (#170)
    Automatic merge from submit-queue.
    
    fix(openapi): fix path item overriding problem
    
    **What this PR does / why we need it**:
    
    Add your description
    
    **Which issue(s) this PR fixes** *(optional, close the issue(s) when PR gets merged)*:
    
    Fixes #137 
    
    **Special notes for your reviewer**:
    
    /cc @ddysher 
    
    **Release note**:
    
    
    ```release-note
    NONE
    ```
    
    <!--  Thanks for sending a pull request! Here are some tips:
    
    1. https://github.com/caicloud/engineering/blob/master/docs/review_conventions.md  <-- what is the review process looks like
    2. https://github.com/caicloud/engineering/blob/master/docs/commit_conventions.md  <-- how to structure your git commit
    3. https://github.com/caicloud/engineering/blob/master/docs/caicloud_bot.md        <-- how to work with caicloud bot
    
    Other tips from Kubernetes cmomunity:
    
    1. If this is your first time, read our contributor guidelines https://git.k8s.io/community/contributors/devel/pull-requests.md#the-pr-submit-process and developer guide https://git.k8s.io/community/contributors/devel/development.md#development-guide
    2. If you want *faster* PR reviews, read how: https://git.k8s.io/community/contributors/devel/pull-requests.md#best-practices-for-faster-reviews
    3. Follow the instructions for writing a release note: https://git.k8s.io/community/contributors/devel/pull-requests.md#write-release-notes-if-needed
    4. If the PR is unfinished, see how to mark it: https://github.com/kubernetes/community/blob/master/contributors/devel/pull-requests.md#marking-unfinished-pull-requests
    -->

--- cmd/openapi-gen/builder/openapi.go
+++ cmd/openapi-gen/builder/openapi.go
@@ -79,16 +79,19 @@ func newOpenAPI(c *common.Config) (*openAPI, error) {
 }
 
 func (o *openAPI) init(descriptor *definition.Descriptor) error {
-	return o.buildDescriptor("", nil, nil, descriptor)
+	specPaths := o.swagger.SwaggerProps.Paths.Paths
+	return o.buildDescriptor(specPaths, "", nil, nil, descriptor)
 }
 
-func (o *openAPI) buildDescriptor(path string, consumes, produces []string, descriptor *definition.Descriptor) error {
+func (o *openAPI) buildDescriptor(specPaths map[string]spec.PathItem, path string, consumes, produces []string, descriptor *definition.Descriptor) error {
 	path = strings.TrimSuffix(path, "/") + descriptor.Path
-	specPaths, err := o.buildPathItems(path, consumes, produces, descriptor.Definitions)
+	pathItem, err := o.buildPathItem(consumes, produces, descriptor.Definitions)
 	if err != nil {
 		return err
 	}
-	o.swagger.SwaggerProps.Paths.Paths = specPaths
+	if pathItem != nil {
+		specPaths[path] = *pathItem
+	}
 	c, p := descriptor.Consumes, descriptor.Produces
 
 	if c == nil {
@@ -99,17 +102,19 @@ func (o *openAPI) buildDescriptor(path string, consumes, produces []string, desc
 	}
 
 	for _, d := range descriptor.Children {
-		if err := o.buildDescriptor(descriptor.Path, c, p, &d); err != nil {
+		if err := o.buildDescriptor(specPaths, descriptor.Path, c, p, &d); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (o *openAPI) buildPathItems(path string, consumes, produces []string, defs []definition.Definition) (map[string]spec.PathItem, error) {
-	specPaths := map[string]spec.PathItem{}
+func (o *openAPI) buildPathItem(consumes, produces []string, defs []definition.Definition) (*spec.PathItem, error) {
+	if len(defs) == 0 {
+		return nil, nil
+	}
+	pathItem := spec.PathItem{}
 	for _, def := range defs {
-		pathItem := spec.PathItem{}
 		op, err := o.buildOperation(&def, consumes, produces, service.HTTPCodeFor(def.Method))
 		if err != nil {
 			return nil, err
@@ -136,9 +141,8 @@ func (o *openAPI) buildPathItems(path string, consumes, produces []string, defs 
 		case definition.AsyncDelete:
 			pathItem.Delete = op
 		}
-		specPaths[path] = pathItem
 	}
-	return specPaths, nil
+	return &pathItem, nil
 }
 
 func (o *openAPI) buildOperation(def *definition.Definition, consumes, produces []string, defaultStatusCode int) (*spec.Operation, error) {
