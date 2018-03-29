5f960237f137177cec4f89188093ea68752cec9e
Wei Guo <me@imkira.com>
2018-03-29 19:05:16 +0800

    feat(config): use table printer to format help message (#194)
    Automatic merge from submit-queue.
    
     feat(config): use table printer to format help message
    
    **What this PR does / why we need it**:
    
    Use table printer to format help message.
    ```
    ➜  testnirvana ./testnirvana --help
    ConfigKey-ENV-Flag Mapping Table
    
       Config Key                   ENV                          Flag                    Current Value
    1  server.ip                    SERVER_IP                    --ip
    2  server.metrics.namespace     SERVER_METRICS_NAMESPACE     --metrics-namespace
    3  server.metrics.path          SERVER_METRICS_PATH          --metrics-path          /metrics
    4  server.port                  SERVER_PORT                  --port                  8080
    5  server.profiling.contention  SERVER_PROFILING_CONTENTION  --profiling-contention  false
    6  server.profiling.path        SERVER_PROFILING_PATH        --profiling-path        /profiling
    
    Usage:
      server [flags]
    
    Flags:
      -h, --help                       help for server
          --ip string                  Nirvana server listening IP
          --metrics-namespace string   Metrics namespace
          --metrics-path string        Metrics path (default "/metrics")
          --port uint16                Nirvana server listening Port (default 8080)
          --profiling-contention       Enable contention profiling
          --profiling-path string      Profiling path (default "/profiling")
    ```
    
    It's more readable than before.
    
    **Which issue(s) this PR fixes** *(optional, close the issue(s) when PR gets merged)*:
    
    Fixes #
    
    **Special notes for your reviewer**:
    
    /cc @zoumo 
    /cc @ddysher 
    
    **Release note**:
    
    
    ```release-note
    NONE
    ```
    
    <!--  Thanks for sending a pull request! Here are some tips:
    
    1. https://github.com/caicloud/engineering/blob/master/docs/review_conventions.md  <-- what is the review process looks like
    2. https://github.com/caicloud/engineering/blob/master/docs/commit_conventions.md  <-- how to structure your git commit
    3. https://github.com/caicloud/engineering/blob/master/docs/caicloud_bot.md        <-- how to work with caicloud bot
    
    Other tips from Kubernetes cmomunity:
    
    1. If this is your first time, read our contributor guidelines https://git.k8s.io/community/contributors/devel/pull-requests.md#the-pr-submit-process and developer guide https://git.k8s.io/community/contributors/devel/development.md#development-guide
    2. If you want *faster* PR reviews, read how: https://git.k8s.io/community/contributors/devel/pull-requests.md#best-practices-for-faster-reviews
    3. Follow the instructions for writing a release note: https://git.k8s.io/community/contributors/devel/pull-requests.md#write-release-notes-if-needed
    4. If the PR is unfinished, see how to mark it: https://github.com/kubernetes/community/blob/master/contributors/devel/pull-requests.md#marking-unfinished-pull-requests
    -->

--- config/config_test.go
+++ config/config_test.go
@@ -17,7 +17,6 @@ limitations under the License.
 package config
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 	"time"
@@ -83,32 +82,32 @@ func TestCommand(t *testing.T) {
 		"sxxx", "sxxxd",
 	}
 	fields := []configField{
-		{&o.HTTP, o.HTTP, "test.http", "TEST_HTTP", "", "test-http", "HTTP"},
-		{&o.HTTPPort, o.HTTPPort, "test.httpPort", "TEST_HTTP_PORT", "", "test-http-port", "HTTPPort"},
-		{&o.HTTPPort2, o.HTTPPort2, "test.httpPort2", "TEST_HTTP_PORT2", "", "test-http-port2", "HTTPPort2"},
-		{&o.HTTPPort233a, o.HTTPPort233a, "test.httpPort233a", "TEST_HTTP_PORT233A", "", "test-http-port233a", "HTTPPort233a"},
-		{&o.HTTPPort233Aa, o.HTTPPort233Aa, "test.httpPort233Aa", "TEST_HTTP_PORT233_AA", "", "test-http-port233-aa", "HTTPPort233Aa"},
-		{&o.HTTP2, o.HTTP2, "test.http2", "TEST_HTTP2", "", "test-http2", "HTTP2"},
-		{&o.HTTP2Port, o.HTTP2Port, "test.http2Port", "TEST_HTTP2_PORT", "", "test-http2-port", "HTTP2Port"},
-		{&o.SomeHTTP, o.SomeHTTP, "test.someHTTP", "TEST_SOME_HTTP", "", "test-some-http", "SomeHTTP"},
-		{&o.SomeHTTPDesc, o.SomeHTTPDesc, "test.someHTTPDesc", "TEST_SOME_HTTP_DESC", "", "test-some-http-desc", "SomeHTTPDesc"},
+		{&o.HTTP, o.HTTP, "server.test.http", "SERVER_TEST_HTTP", "", "test-http", "HTTP"},
+		{&o.HTTPPort, o.HTTPPort, "server.test.httpPort", "SERVER_TEST_HTTP_PORT", "", "test-http-port", "HTTPPort"},
+		{&o.HTTPPort2, o.HTTPPort2, "server.test.httpPort2", "SERVER_TEST_HTTP_PORT2", "", "test-http-port2", "HTTPPort2"},
+		{&o.HTTPPort233a, o.HTTPPort233a, "server.test.httpPort233a", "SERVER_TEST_HTTP_PORT233A", "", "test-http-port233a", "HTTPPort233a"},
+		{&o.HTTPPort233Aa, o.HTTPPort233Aa, "server.test.httpPort233Aa", "SERVER_TEST_HTTP_PORT233_AA", "", "test-http-port233-aa", "HTTPPort233Aa"},
+		{&o.HTTP2, o.HTTP2, "server.test.http2", "SERVER_TEST_HTTP2", "", "test-http2", "HTTP2"},
+		{&o.HTTP2Port, o.HTTP2Port, "server.test.http2Port", "SERVER_TEST_HTTP2_PORT", "", "test-http2-port", "HTTP2Port"},
+		{&o.SomeHTTP, o.SomeHTTP, "server.test.someHTTP", "SERVER_TEST_SOME_HTTP", "", "test-some-http", "SomeHTTP"},
+		{&o.SomeHTTPDesc, o.SomeHTTPDesc, "server.test.someHTTPDesc", "SERVER_TEST_SOME_HTTP_DESC", "", "test-some-http-desc", "SomeHTTPDesc"},
 
-		{&o.Int8, o.Int8, "test.int8", "TEST_INT8", "", "test-int8", "Int8"},
-		{&o.Int16, o.Int16, "test.int16", "TEST_INT16", "", "test-int16", "Int16"},
-		{&o.Int32, o.Int32, "test.int32", "TEST_INT32", "", "test-int32", "Int32"},
-		{&o.Int64, o.Int64, "test.int64", "TEST_INT64", "", "test-int64", "Int64"},
-		{&o.Int, o.Int, "test.int", "TEST_INT", "", "test-int", "Int"},
-		{&o.Uint8, o.Uint8, "test.uint8", "TEST_UINT8", "", "test-uint8", "Uint8"},
-		{&o.Uint16, o.Uint16, "test.uint16", "TEST_UINT16", "", "test-uint16", "Uint16"},
-		{&o.Uint32, o.Uint32, "test.uint32", "TEST_UINT32", "", "test-uint32", "Uint32"},
-		{&o.Uint64, o.Uint64, "test.uint64", "TEST_UINT64", "", "test-uint64", "Uint64"},
-		{&o.Uint, o.Uint, "test.uint", "TEST_UINT", "", "test-uint", "Uint"},
-		{&o.Float32, o.Float32, "test.float32", "TEST_FLOAT32", "", "test-float32", "Float32"},
-		{&o.Float64, o.Float64, "test.float64", "TEST_FLOAT64", "", "test-float64", "Float64"},
-		{&o.String, o.String, "test.string", "TEST_STRING", "", "test-string", "String"},
-		{&o.StringSlice, o.StringSlice, "test.stringSlice", "TEST_STRING_SLICE", "", "test-string-slice", "StringSlice"},
-		{&o.Bool, o.Bool, "test.bool", "TEST_BOOL", "", "test-bool", "Bool"},
-		{&o.Duration, o.Duration, "test.duration", "TEST_DURATION", "", "test-duration", "Duration"},
+		{&o.Int8, o.Int8, "server.test.int8", "SERVER_TEST_INT8", "", "test-int8", "Int8"},
+		{&o.Int16, o.Int16, "server.test.int16", "SERVER_TEST_INT16", "", "test-int16", "Int16"},
+		{&o.Int32, o.Int32, "server.test.int32", "SERVER_TEST_INT32", "", "test-int32", "Int32"},
+		{&o.Int64, o.Int64, "server.test.int64", "SERVER_TEST_INT64", "", "test-int64", "Int64"},
+		{&o.Int, o.Int, "server.test.int", "SERVER_TEST_INT", "", "test-int", "Int"},
+		{&o.Uint8, o.Uint8, "server.test.uint8", "SERVER_TEST_UINT8", "", "test-uint8", "Uint8"},
+		{&o.Uint16, o.Uint16, "server.test.uint16", "SERVER_TEST_UINT16", "", "test-uint16", "Uint16"},
+		{&o.Uint32, o.Uint32, "server.test.uint32", "SERVER_TEST_UINT32", "", "test-uint32", "Uint32"},
+		{&o.Uint64, o.Uint64, "server.test.uint64", "SERVER_TEST_UINT64", "", "test-uint64", "Uint64"},
+		{&o.Uint, o.Uint, "server.test.uint", "SERVER_TEST_UINT", "", "test-uint", "Uint"},
+		{&o.Float32, o.Float32, "server.test.float32", "SERVER_TEST_FLOAT32", "", "test-float32", "Float32"},
+		{&o.Float64, o.Float64, "server.test.float64", "SERVER_TEST_FLOAT64", "", "test-float64", "Float64"},
+		{&o.String, o.String, "server.test.string", "SERVER_TEST_STRING", "", "test-string", "String"},
+		{&o.StringSlice, o.StringSlice, "server.test.stringSlice", "SERVER_TEST_STRING_SLICE", "", "test-string-slice", "StringSlice"},
+		{&o.Bool, o.Bool, "server.test.bool", "SERVER_TEST_BOOL", "", "test-bool", "Bool"},
+		{&o.Duration, o.Duration, "server.test.duration", "SERVER_TEST_DURATION", "", "test-duration", "Duration"},
 	}
 	cmd := NewDefaultNirvanaCommand()
 	cmd.EnablePlugin(o)
@@ -145,7 +144,7 @@ func TestCommand(t *testing.T) {
 		if flag.Name != f.longFlag {
 			t.Fatalf("Inequality flag name for %s: %v", f.key, flag.Name)
 		}
-		if flag.Usage != fmt.Sprintf("%s (env %s) (cfg %s)", f.description, f.env, f.key) {
+		if flag.Usage != f.description {
 			t.Fatalf("Inequality flag usage for %s: %v", f.key, flag.Usage)
 		}
 	}
@@ -153,23 +152,20 @@ func TestCommand(t *testing.T) {
 
 func TestChooseValue(t *testing.T) {
 	values := []struct {
-		key          string
 		value        interface{}
-		env          string
 		envValue     interface{}
 		defaultValue interface{}
-		desc         string
 		result       interface{}
 	}{
-		{"test.key", 1, "TEST_KEY", 2, 3, " (env TEST_KEY=2) (cfg test.key=1)", 2},
-		{"test.key", 1, "TEST_KEY", nil, 3, " (env TEST_KEY) (cfg test.key=1)", 1},
-		{"test.key", nil, "TEST_KEY", 2, 3, " (env TEST_KEY=2) (cfg test.key)", 2},
-		{"test.key", nil, "TEST_KEY", nil, 3, " (env TEST_KEY) (cfg test.key)", 3},
+		{1, 2, 3, 2},
+		{1, nil, 3, 1},
+		{nil, 2, 3, 2},
+		{nil, nil, 3, 3},
 	}
 	for _, v := range values {
-		desc, result := chooseValue(v.key, v.value, v.env, v.envValue, v.defaultValue)
-		if desc != v.desc || result != v.result {
-			t.Fatalf("chooseValue generates wrong results: %s, %v", desc, result)
+		result := chooseValue(v.value, v.envValue, v.defaultValue)
+		if result != v.result {
+			t.Fatalf("chooseValue generates wrong results: %v", result)
 		}
 	}
 }
