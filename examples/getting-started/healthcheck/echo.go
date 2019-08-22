8198536dc7cd579279fb3d9d649a7cd7249c5fb2
iawia002 <xuxinzhao@caicloud.io>
2019-08-22 11:49:55 +0800

    feat(plugin): fix ci

--- examples/getting-started/healthcheck/echo.go
+++ examples/getting-started/healthcheck/echo.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Caicloud Authors
+Copyright 2018 Caicloud Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
