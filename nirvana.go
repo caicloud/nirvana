2ef92ca6778b5b79e8d98ac1ef7f8c54fb62a2e0
Xinzhao Xu <xuxinzhao@caicloud.io>
2020-11-13 16:41:37 +0800

    fix(*): add the missing blank import in nirvana.go (#378)

--- nirvana.go
+++ nirvana.go
@@ -27,6 +27,10 @@ import (
 	"github.com/caicloud/nirvana/errors"
 	"github.com/caicloud/nirvana/log"
 	"github.com/caicloud/nirvana/service"
+
+	// This blank import will make it in the dependencies of projects using Nirvana
+	// for API docs generation.
+	_ "github.com/caicloud/nirvana/utils/api"
 )
 
 // Server is a complete API server.
