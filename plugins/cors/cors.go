e90bcff9e80d619ee90e7cb1ef47cedb9bbb630b
Rim <gero.rim@gmail.com>
2021-01-22 17:22:03 +0800

    fix(boilerplate): change date from 2021 to 2017

--- plugins/cors/cors.go
+++ plugins/cors/cors.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Caicloud Authors
+Copyright 2017 Caicloud Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
